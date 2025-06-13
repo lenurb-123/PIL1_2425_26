7c0bc7f9df807fac879cdfdfc1d13e739977cc81
Brunel <kpogbemebrunel@gmail.com>
2025-06-13 18:31:20 +0100

    Ajout du dossier static aux app

--- IFRI_comotorage/IFRI_comotorage/settings.py
+++ IFRI_comotorage/IFRI_comotorage/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Comotorage',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'azerty',
         'HOST': 'localhost',
         'PORT': '3306',
     }
